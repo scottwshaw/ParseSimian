b2acbbabf881ed9a50d5970630d58ca5fc4da6ab
Scott Shaw <scottwshaw@gmail.com>
2011-06-05 22:44:11 -0500

    changed use into require

--- src/parse_simian.clj
+++ src/parse_simian.clj
@@ -1,16 +1,16 @@
 (ns parse-simian
-  (:use [clojure.contrib.json :only [json-str pprint-json]]
-	[clojure.contrib.io :only [input-stream]]
-  	[clojure.contrib.command-line :only [with-command-line]]
-	[parse-simian.core :only [parse-simian-report]])
-  (:require [clojure.string :as str]))
-
+  (:require [clojure.contrib
+	     [json :as json]
+	     [io :as io]
+	     [command-line :as cl]]
+	    [parse-simian.core :as ps]
+	    [clojure.string :as str]))
 
 (defn -main [& args]
-  (with-command-line args
+  (cl/with-command-line args
     "parse an xml file and write graph structure to json"
     [filevec]
     (let [[filename & frest] filevec
 	  [rootname & srest] (str/split filename #"\.")]
-      (print "var " rootname " = " (json-str (parse-simian-report (input-stream filename))) ";"))))
+      (print "var " rootname " = " (json/json-str (ps/parse-simian-report (io/input-stream filename))) ";"))))
 
